a5876e4387297e69352af0cd268af6dc02f3e579
David Weber <dave@veryflatcat.com>
2022-02-03 15:56:01 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	build.sbt
    #	project/build.properties
    #	src/main/scala/demo/Five.scala
    #	src/main/scala/demo/Seven.scala
    #	src/main/scala/demo/Six.scala
    #	src/main/scala/demo/Two.scala

--- src/main/scala/demo/FizzBuzz.scala
+++ src/main/scala/demo/FizzBuzz.scala
@@ -1,18 +1,10 @@
 package demo
 
-object FizzBuzz {
-
-  def main(args: Array[String]): Unit = {
-
-    (1 to 100)
-      .map { i =>
-        (i % 3, i % 5) match {
-          case (0, 0) => "FizzBuzz"
-          case (0, _) => "Fizz"
-          case (_, 0) => "Buzz"
-          case _      => i.toString
-        }
-      }
-      .foreach(println)
-  }
-}
+@main def fizzbuzz(): Unit =
+  (1 to 100).map { i =>
+    (i % 3, i % 5) match
+      case (0, 0) => "FizzBuzz"
+      case (0, _) => "Fizz"
+      case (_, 0) => "Buzz"
+      case _ => i.toString
+  }.foreach(println)
